ae10bf3c4749b486867d6e5cce2fa464dd70864c
ragin <m.g.mueller@yandex.com>
2020-09-06 17:55:29 +0200

    new test of pricenotifications

---
+++ src/test/java/de/concepts/kadis/PriceNotificationTest.java
@@ -0,0 +1,12 @@
+package de.concepts.kadis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriceNotificationTest {
+
+    @Test
+    void testToString() {
+    }
+}
