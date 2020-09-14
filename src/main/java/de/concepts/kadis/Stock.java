3e7e69dd2fc948eede8d3212800535e412bd730b
ragin <m.g.mueller@yandex.com>
2020-09-14 10:26:12 +0200

    new business object

---
+++ src/main/java/de/concepts/kadis/Stock.java
@@ -0,0 +1,45 @@
+/* Copyright (C) Marc Müller - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Marc Müller <marc@mgm.technology>, 2020
+ */
+package de.concepts.kadis;
+
+public class Stock {
+    String articleNumber;
+    Integer stock;
+    Integer reserved;
+
+    public Stock() {
+    }
+
+    public Stock(String articleNumber, Integer stock, Integer reserved) {
+        this.articleNumber = articleNumber;
+        this.stock = stock;
+        this.reserved = reserved;
+    }
+
+    public String getArticleNumber() {
+        return articleNumber;
+    }
+
+    public void setArticleNumber(String articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getReserved() {
+        return reserved;
+    }
+
+    public void setReserved(Integer reserved) {
+        this.reserved = reserved;
+    }
+}
